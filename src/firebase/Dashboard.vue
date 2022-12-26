3b9d37c3eb4df5fa57a6c15a65045b3b5291c218
anaskhonprime <smlife202@gmail.com>
2022-12-26 21:03:09 +0900

    Updated Components

--- src/firebase/Dashboard.vue
+++ src/firebase/Dashboard.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="container">
+        <h1>Welcome </h1>
+    </div>
 </template>
 
 <script>
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     lintOnSave: false,
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/personal-portfolio/' : '/',
+});
 
-})
+module.exports = ({
+
+    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+});
