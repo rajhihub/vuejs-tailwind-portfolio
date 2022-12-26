78ab268689eed4982075e6fc47496846491a0320
anaskhonprime <smlife202@gmail.com>
2022-12-26 21:06:10 +0900

    Updated Files

--- src/firebase/Dashboard.vue
+++ src/firebase/Dashboard.vue
@@ -1,7 +1,49 @@
 <template>
-    <div class="container">
-        <h1>Welcome </h1>
-    </div>
+   <span class="bckg"></span>
+<header>
+  <h1>Dashboard</h1>
+  <nav>
+    <ul>
+      <li>
+        <a href="javascript:void(0);" data-title="Projects">Projects</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="Team">Team</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="Diary">Diary</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="Timeline">Timeline</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="Settings">Settings</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="Search">Search</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+<main>
+  <div class="title">
+    <h2>Projects</h2>
+    <a href="javascript:void(0);">Hello Bob !</a>
+  </div>
+
+  <article class="larg">
+    <div>
+      <h3>Project 1 <span class="entypo-down-open"></span></h3>
+      </div>
+    <div>
+      <h3>Project 2 <span class="entypo-down-open"></span></h3>
+       </div>
+    <div>
+      <h3>Project 3 <span class="entypo-down-open"></span></h3>
+       </div>
+  </article>
+</main>
+
 </template>
 
 <script>
