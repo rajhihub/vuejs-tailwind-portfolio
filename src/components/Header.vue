9e496a95915773c190f20b36455df132d285402a
rajhihub <124029225+rajhihub@users.noreply.github.com>
2023-01-31 22:59:06 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,11 +10,11 @@
               :enter="{ opacity: 1, x: 0 }"
               :delay="500"
               class="text-green-600 hover:text-green-500 text-2xl lg:text-4xl font-medium lg:font-semibold mb-5 lg:leading-normal leading-snug">
-              HI, I'm ANASKHON
+              Hai, Wargi 
               <br />
-              Front-End Developer,
+              Desa Wanaherang,
               <br />
-              Blockchain Engineer
+              Kecamatan Gunung Putri
             </p>
             <p
               v-motion
@@ -22,8 +22,7 @@
               :enter="{ opacity: 1, y: 0 }"
               :delay="1200"
               class="hidden lg:block text-2xl mb-5">
-              I like to craft solid and scalable frontend products with great
-              user experiences.
+             Mari Kita Majukan Desa Tanah kelahiran kita semua.
             </p>
 
             <router-link
@@ -34,7 +33,7 @@
               to="/about"
               name="fade"
               class="btn btn-outline border text-secondary">
-              About
+              Tentang
             </router-link>
             <a
               href="#"
@@ -43,24 +42,12 @@
               :enter="{ opacity: 1, y: 0 }"
               :delay="1600"
               class="btn btn-primary btn-split ml-2">
-              Resume
+              Video
               <div class="fab"><span class="mai-play"></span></div>
             </a>
           </div>
           <div class="col-lg-6 wow zoomIn">
-            <div
-              v-motion
-              :initial="{ opacity: 0, x: 200 }"
-              :enter="{ opacity: 1, x: 0 }"
-              :delay="1400"
-              class="m-3 lg:m-3 p-1 lg:p-5">
-              <lottie-player
-                src="https://assets2.lottiefiles.com/packages/lf20_gnb0jsok.json"
-                background="transparent"
-                speed="1"
-                style="max-width: 500px; max-height: 500px"
-                autoplay></lottie-player>
-            </div>
+          
           </div>
         </div>
       </div>
